97e16239650efab0aa215d23c92fe1983e76b481
pegasus-yang <panesas2@163.com>
2023-05-03 10:00:17 +0800

    重新梳理目录结构，增加设计原则部分 重新梳理工厂模式，完善实例代码和PPT

--- 设计模式/工厂模式/重构代码.py
+++ 设计模式/工厂模式/重构代码.py
@@ -3,34 +3,46 @@
   @Date: 2023/2/14
   @Desc: 
 """
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 
 
 @dataclass
-class Issue:
-    """Issue"""
+class Issue(metaclass=ABCMeta):
+    """
+    Issue模型基类，定义了Issue的基本操作和基本属性
+    """
     id: int = None
     subject: str = None
     database_conn: str = None
 
+    @abstractmethod
     def save(self):
-        raise NotImplementedError
+        ...
 
+    @abstractmethod
     def update(self, **kwargs):
-        raise NotImplementedError
+        ...
 
+    @abstractmethod
     def search(self, **kwargs):
-        raise NotImplementedError
+        ...
 
+    @abstractmethod
     def delete(self):
-        raise NotImplementedError
+        ...
 
 
 @dataclass
 class RedmineIssue(Issue):
-    """使用redmine存储Issue"""
+    """
+    在Redmine中存储Issue的实现类，继承了Issue基类，包含了针对Redmine进行处理的特定方法
+    """
 
     def get_value_from_custom_field(self):
+        """
+        Redmine特有的数据处理方法
+        """
         ...
 
     def save(self):
@@ -48,7 +60,15 @@ class RedmineIssue(Issue):
 
 @dataclass
 class MysqlIssue(Issue):
-    """使用mysql存储Issue"""
+    """
+    在Mysql中存储Issue的实现类，继承了Issue基类，包含了针对Mysql进行处理的特定方法
+    """
+
+    def connect_to_database(self):
+        """
+        Mysql特有的数据处理方法
+        """
+        pass
 
     def save(self):
         pass
@@ -64,36 +84,54 @@ class MysqlIssue(Issue):
 
 
 class IssueFactory:
+    """
+    工厂的抽象基类，用于规范具体工厂需要实现的方法
+    """
     database = None
 
     def __init__(self, database):
         self.database = database
 
+    @abstractmethod
+    def get_issue(self, **kwargs) -> Issue:
+        """
+        实例化issue
+        """
+        ...
+
+
+class RedmineIssueFactory(IssueFactory):
+    """
+    RedmineIssue的工厂类，用符合基类规范的方式生产RedmineIssue对象
+    """
+
     def find_in_storage(self, **kwargs) -> Issue:
         """
-        从存储中加载数据
+        从缓存中读取数据
         """
         ...
 
     def get_issue(self, **kwargs) -> Issue:
         """
-        实例化issue
+        通过重写父类的方法，实现具体的工厂
         """
-        return Issue()
-
-
-class RedmineIssueFactory(IssueFactory):
-
-    def get_issue(self, **kwargs) -> Issue:
         instance = self.find_in_storage(**kwargs)
         if instance:
+            instance.database_conn = self.database
             return instance
         else:
             return RedmineIssue(database_conn=self.database, **kwargs)
 
 
 class MysqlIssueFactory(IssueFactory):
+    """
+    MysqlIssue的工厂类，用符合基类规范的方式生产MysqlIssue对象
+    """
+
     def get_issue(self, **kwargs) -> Issue:
+        """
+        通过重写父类的方法，实现具体的工厂
+        """
         return MysqlIssue(database_conn=self.database, **kwargs)
 
 
@@ -107,3 +145,5 @@ if __name__ == '__main__':
 
     r_issue.save()
     m_issue.update()
+    # IDE不会提示RedmineIssue的特有方法，并且在使用特有方法时出现警告，避免误用实现细节相关的方法
+    r_issue.find_in_storage()
